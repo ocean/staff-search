4fa65dc99981270961d52f1271c721a832bfb2a6
Drew Robinson <drew.robinson@gmail.com>
2018-06-28 12:47:19 +0800

    Add some more tests.

--- test/unit/specs/StaffSearch.spec.js
+++ test/unit/specs/StaffSearch.spec.js
@@ -5,6 +5,12 @@ describe('StaffSearch.vue', () => {
   it('has a created hook', () => {
     expect(typeof StaffSearch.created).toBe('function');
   });
+  it('has a mounted hook', () => {
+    expect(typeof StaffSearch.mounted).toBe('function');
+  });
+  it('has child components', () => {
+    expect(typeof StaffSearch.components).toBe('object');
+  });
   it('should render correct contents', () => {
     const Constructor = Vue.extend(StaffSearch);
     const vm = new Constructor().$mount();
